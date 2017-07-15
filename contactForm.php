8a69d1be7fc7930387e3ddc3bf9ae827443d0e07
JonathanMcNelly <Nathalie.Kranich@t-online.de>
2017-07-15 17:03:01 +0100

    update

---
+++ contactForm.php
@@ -0,0 +1,12 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = "Nathalie.Kranich@Gmail.com"; // this is your Email address
+    $from = $_POST['e-mail']; // this is the sender's Email address
+    $first_name = $_POST['firstname'];
+    $last_name = $_POST['lastname'];
+    $subject =$_POST['subject'];
+    $message = $_POST['message'];
+
+    mail($to,$subject,$message,$headers);
+    }
+?>
